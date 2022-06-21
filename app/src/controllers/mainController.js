6f17a26f73a3650e8c6b166cd4346466d427c68b
VilchezMax <maximiliano.vilchez@outlook.com>
2022-06-20 22:49:54 -0300

    Separe views en user y products. Modifique rutas para llegar a esas vistas en funcion de eso

--- app/src/controllers/mainController.js
+++ app/src/controllers/mainController.js
@@ -2,31 +2,31 @@
 //Seguramente tengamos que dividir en userControllers y productControllers
 mainController={
     index: (req, res) => {
-        res.render('index');
+        res.render('users/index');
     },
     
     producto: (req, res) => {
-        res.render('productDetail');
+        res.render('users/productDetail');
     },
     
     carrito: (req, res) => {
-        res.render('productCart');
+        res.render('products/productCart');
     },
     
     login: (req, res) => {
-        res.render('login');
+        res.render('users/login');
     },
 
     register: (req, res) => {
-        res.render('register');
+        res.render('users/register');
     },
 
     create: (req, res) => {
-        res.render('productCreate');
+        res.render('products/productCreate');
     },
 
     edit: (req, res) => {
-        res.render('productEdit');
+        res.render('products/productEdit');
     }
 
 }
