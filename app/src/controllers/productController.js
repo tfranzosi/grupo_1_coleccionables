c6cfbc4c9c312478a715383a60c16b88e056bf1c
VilchezMax <maximiliano.vilchez@outlook.com>
2022-06-27 14:11:06 -0300

    Agregue en controllers metodo edit que se debera llenar, y modifique edit anterior porque reacciona al get y muestra el form, ahora se llama -editForm

--- app/src/controllers/productController.js
+++ app/src/controllers/productController.js
@@ -1,4 +1,9 @@
-const db = require('../../public/db/db.json');
+const fs = require('fs');
+const path = require('path');
+const rutaDB = path.join(__dirname,'../../public/db/db.json');
+const db = require(rutaDB);
+const readDB = fs.readFileSync(rutaDB,'utf-8');
+const dbparseada = JSON.parse(readDB);
 
 productController={
     producto: (req, res) => {
@@ -6,18 +11,45 @@ productController={
     },
     
     create: (req, res) => {
-        res.render('products/productCreate');
+        res.render('products/productCreate',{producto: db});
     },
 
-    edit: (req, res) => {
+    editForm: (req, res) => {
         res.render('products/productEdit',{producto: productController.buscarProductoPorId(parseInt(req.params.id))});
     },
-    buscarProductoPorId: function (id) {
+    buscarProductoPorId: function (id) { //FALTA EDITAR PARA QUE DEVUELVA INDEX, COMO HABLAMOS EN LLAMADA
         return db.completa.find(producto => {
             return producto.sku === id;
         });
-    }
+    },
+    edit: (req,res) => { //MV: PUShEO LA BASE PERO LUEGO LA IMPLEMENTO
+        let id = req.params.id;
+        /*
 
+        let productoEditado = {
+            sku:req.body.SKU,
+            titulo:req.body.titulo,
+            descripcionCorta:req.body.short_desc,
+            descripcionLarga:req.body.long_desc,
+            precioRegular:req.body.normal_price,
+            precioOferta:req.body.offer_price,
+            descuento:req.body.discount,
+            cantidadCuotas:req.body.cuotas,
+            valorCuota:req.body.cuotas_value,
+            esOferta:req.body.atributo_precio,
+            esFisico:req.body.atributo_estado,
+            categorias:req.body.categories,
+            urlImagen:req.body.imagen
+        }
+        
+        // for(let atributo in readDBparseada.completa[indice]){
+        //     if readDBparseada.completa[indice].atributo === productoEditado.atributo
+        // }
+        
+        readDBparsed.completa[id-1]=productoEditado;
+        fs.writeFileSync(rutaDB,JSON.stringify(readDBparsed,null,2));    */ 
+        res.redirect(`/productos/sku/${id}`);  
+    }
 }
 
 module.exports = productController;
