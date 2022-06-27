749d17464ab19dce535a4c33dd635f5430af50f3
VilchezMax <maximiliano.vilchez@outlook.com>
2022-06-27 13:48:15 -0300

    MV: APP.JS reformateado, agregue method-override y otros middlewares para leer json

--- app/app.js
+++ app/app.js
@@ -1,24 +1,33 @@
+//Modulos
 const express = require('express');
 const app  = express();
 const path = require('path');
+const methodOverride = require('method-override');
+// Routers
 const mainRouter = require('./src/routes/mainRouter');
 const productRouter = require('./src/routes/productRouter');
 const userRouter = require('./src/routes/userRouter');
 
 const PUERTO = 3000;
 
+// MIDDLEWARES
 app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 
 
+// Inicio Servidor
 app.listen(process.env.PORT || PUERTO, () => {
     console.log(`Up & Running en http://127.0.0.1:${PUERTO}`);
 });
 
-
+// View Engine
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,'/src/views'))
+app.set('views', path.join(__dirname,'/src/views'));
 
-//      ---     INICIO DE LAS RUTAS    ---
+
+// INICIO DE LAS RUTAS
 app.use('/', mainRouter);
 app.use('/productos', productRouter);
 app.use('/usuarios', userRouter);
