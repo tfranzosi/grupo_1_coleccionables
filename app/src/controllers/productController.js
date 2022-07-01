bc9923935fcbdf091875a71b91ef919c26746f39
VilchezMax <maximiliano.vilchez@outlook.com>
2022-07-01 01:44:23 -0300

    Cambios parciales en controller. commiteo para poder mergear cambios de pablo

--- app/src/controllers/productController.js
+++ app/src/controllers/productController.js
@@ -10,64 +10,73 @@ productController={
     },
     producto: (req, res) => {
         let id = parseInt(req.params.id);
-        let indice = productController.buscarProductoPorId(id);
+        let indice = productController.buscarIndiceProductoPorId(id);
         res.render('products/productDetail',{producto: dbParseada[indice]});
     },
     
     create: (req, res) => {
         let id = parseInt(req.params.id);
-        let indice = productController.buscarProductoPorId(id);
+        let indice = productController.buscarIndiceProductoPorId(id);
         res.render('products/productCreate',{productos: dbParseada});
     },
 
     editForm: (req, res) => {
         let id = parseInt(req.params.id);
-        let indice = productController.buscarProductoPorId(id);
+        let indice = productController.buscarIndiceProductoPorId(id);
         res.render('products/productEdit',{producto: dbParseada[indice]});
+    },
+
+    edit: (req,res) => { 
+        let idProd = parseInt(req.params.id);
+        let indice = productController.buscarIndiceProductoPorId(idProd);
+        let visitasProd = dbParseada[indice].visitas;
+        let vendidosProd = dbParseada[indice].vendidos;
+        let esMasVendidoProd = dbParseada[indice].esMasVendido;
+        let esOferta = req.body.esOferta; 
+        if (esOferta !== true){esOferta=false}else{esOferta=true};
+        let esFisico = req.body.esFisico; 
+        if (esFisico !== true){esFisico=false}else{esFisico=true};
+        requesttt=req.body
+        console.log(requesttt)
+        //HAY QUE HACER VALIDACIONES AFUERA Y ADENTRO DECLARAR LAS VARIABLES MEJOR!!!!!!!!1
+        //Ejemplo: en etiquetas usar metodos para separar por comas y pushear a un array
+        let productoEditado = { 
+            id:idProd, //int
+            sku: req.body.sku, //str
+            titulo: req.body.titulo, //str
+            descripcionCorta: req.body.descripcionCorta, //str
+            descripcionLarga: req.body.descripcionLarga,  //str
+            precioRegular: parseInt(req.body.precioRegular), //int
+            descuento: req.body.descuento, //bool
+            cantidadCuotas: parseInt(req.body.cantidadCuotas), //int
+            esOferta: esOferta, //bool
+            esFisico: esFisico, //bool
+            etiquetas: req.body.etiquetas,  //str Deberia ser array? o se muestra con substrings separados x coma?
+            categorias: req.body.categories, //str Array
+            urlImagen: req.body.Imagen, //str
+            visitas: visitasProd, //int             /*Implementar mecanismos de contador*/
+            vendidos: vendidosProd, //int           /*Implementar mecanismos de contador*/
+            esMasVendido: esMasVendidoProd //bool   /*Implementar mecanismo dependiente de anterior atributo*/
+        }
+        console.log(productoEditado);
+        dbParseada[indice]=productoEditado;
+        fs.writeFileSync(rutaDB,JSON.stringify(dbParseada,null,2),"utf-8");
+        res.redirect(`/productos/${idProd}`);  
     },
 
     delete: (req, res) => {
         let id = parseInt(req.params.id);
-        let indice = productController.buscarProductoPorId(id);
+        let indice = productController.buscarIndiceProductoPorId(id);
         dbParseada.splice(indice,1);
         fs.writeFileSync(rutaDB,JSON.stringify(dbParseada,null,3));
         res.redirect('/productos');
     
     },
 
-    buscarProductoPorId: function (id) { //devuelvo indice del producto en productdb
+    buscarIndiceProductoPorId: function (id) { //devuelvo indice del producto en productdb
         return dbParseada.findIndex(producto => {
             return producto.id === id;
         });          
-    },
-
-    edit: (req,res) => { //MV: PUShEO LA BASE PERO LUEGO LA IMPLEMENTO
-        let id = req.params.id;
-        /*
-
-        let productoEditado = {
-            sku:req.body.SKU,
-            titulo:req.body.titulo,
-            descripcionCorta:req.body.short_desc,
-            descripcionLarga:req.body.long_desc,
-            precioRegular:req.body.normal_price,
-            precioOferta:req.body.offer_price,
-            descuento:req.body.discount,
-            cantidadCuotas:req.body.cuotas,
-            valorCuota:req.body.cuotas_value,
-            esOferta:req.body.atributo_precio,
-            esFisico:req.body.atributo_estado,
-            categorias:req.body.categories,
-            urlImagen:req.body.imagen
-        }
-        
-        // for(let atributo in readDBparseada.completa[indice]){
-        //     if readDBparseada.completa[indice].atributo === productoEditado.atributo
-        // }
-        
-        readDBparsed.completa[id-1]=productoEditado;
-        fs.writeFileSync(rutaDB,JSON.stringify(readDBparsed,null,2));    */ 
-        res.redirect(`/productos/sku/${id}`);  
     }
 }
 
