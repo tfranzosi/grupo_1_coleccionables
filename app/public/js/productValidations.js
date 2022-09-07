fcc2d3daeb8474e6af8b2c25b381aabee66c3cfe
pgianferro <pablogianferro@gmail.com>
2022-09-06 23:15:30 -0300

    PG: actualización validaciones front para form creación de producto

--- app/public/js/productValidations.js
+++ app/public/js/productValidations.js
@@ -1,3 +1,65 @@
+let validations = {
+    sku: (inputItem, messageItem) => {
+        if (inputItem.value == "") displayMessage(messageItem,'El SKU no puede estar vacío');
+        else if (inputItem.value.length < 6) displayMessage(messageItem,'El SKU no puede tener menos de 6 caracteres');
+        else deleteError(messageItem);
+    },
+
+    title: (inputItem, messageItem) => {
+        if (inputItem.value == "") displayMessage(messageItem,'El título no puede estar vacío');
+        else if (inputItem.value.length < 5) displayMessage(messageItem,'El título no puede tener menos de 5 caracteres');
+        else deleteError(messageItem);
+    },
+
+    shortDescription: (inputItem, messageItem) => {
+        if (inputItem.value == "") displayMessage(messageItem,'La descripción no puede estar vacía');
+        else if (inputItem.value.length < 20) displayMessage(messageItem,'La descripción no puede tener menos de 20 caracteres');
+        else deleteError(messageItem);
+    },
+
+    longDescription: (inputItem, messageItem) => {
+        if (inputItem.value == "") displayMessage(messageItem,'La descripción no puede estar vacía');
+        else if (inputItem.value.length < 20) displayMessage(messageItem,'La descripción no puede tener menos de 20 caracteres');
+        else deleteError(messageItem);
+    },
+
+    regularPrice: (inputItem, messageItem) => {
+        if (inputItem.value == "") displayMessage(messageItem,'El precio no puede estar vacío');
+        else if (parseInt(inputItem.value) < 0) displayMessage(messageItem,'El precio no puede ser negativo');
+        else deleteError(messageItem);
+    },
+
+    discount: (inputItem, messageItem) => {
+        if (parseInt(inputItem.value) < 0) displayMessage(messageItem,'El descuento no puede ser negativo');
+        else deleteError(messageItem);
+    },
+
+    fee_q: (inputItem, messageItem) => {
+        if (parseInt(inputItem.value) < 0) displayMessage(messageItem,'Las cuotas no pueden ser negativo');
+        else deleteError(messageItem);
+    },
+
+    tags: (inputItem, messageItem) => {
+        if (inputItem.value == "") displayMessage(messageItem,'Debes ingresar al menos un tag');
+        else deleteError(messageItem);
+    },
+
+    multiple: (inputItem, messageItem) => {
+        let isChecked = false;
+        inputItem.forEach( select => {
+                if ( select.checked ) isChecked = true
+            } );
+        if (!isChecked) displayMessage(messageItem,'Debes seleccionar una categoría');
+        else deleteError(messageItem);
+    },
+
+    image: (inputItem, messageItem) => {
+        let imageAllowed = inputItem.value.match(/(\.jpg|\.jpeg|\.png|\.gif)$/) !== null;
+            if ( !imageAllowed && inputItem.value !== '' ) displayMessage(messageItem,'La imagen debe ser en formato .jpeg, .jpg, .gif o .png');
+            else deleteError(messageItem);
+        },
+}
+
 window.addEventListener('load', () => {
     //Capturamos el formulario
     const form = document.querySelector("form")
@@ -5,200 +67,85 @@ window.addEventListener('load', () => {
     //Capturamos los campos del formulario y los "smalls" que mostraran los mensajes de error
     const inputSku= document.querySelector("#sku")
     const errorSku= document.querySelector("#errorSku")
+
     const inputTitle= document.querySelector("#titulo")
     const errorTitle= document.querySelector("#errorTitle")
+
     const inputShortDescription= document.querySelector("#descripcionCorta")
     const errorShortDescription= document.querySelector("#errorShortDescription")
+
     const inputLongDescription= document.querySelector("#descripcionLarga")
     const errorLongDescription= document.querySelector("#errorLongDescription")
+
     const inputRegularPrice= document.querySelector("#precioRegular")
     const errorRegularPrice= document.querySelector("#errorRegularPrice")
+
     const inputDiscount= document.querySelector("#descuento")
     const errorDiscount= document.querySelector("#errorDiscount")
+
     const inputFee_q= document.querySelector("#cantidadCuotas")
     const errorFee_q= document.querySelector("#errorFee_q")
+
     const inputTags= document.querySelector("#etiquetas")
     const errorTags= document.querySelector("#errorTags")
-    const inputIs_physical= document.querySelector("#esFisico")
-    const inputIs_digital= document.querySelector("#esDigital")
-    const errorIs_physical= document.querySelector("#errorIs_physical")
+
     const inputCategories= document.querySelector(".categories")
     const errorCategories= document.querySelector("#errorCategories")
+
     const inputImage_url= document.querySelector("#fotoPerfil")
     const errorImage_url= document.querySelector("#errorImage_url")
 
+    const allInputs = document.querySelectorAll('form input');
+    // const allTextAreas = document.querySelectorAll('form.registro textarea');
 
 
     //Primer campo donde queremos que se posicione el usuario al cargar la página
     inputSku.focus()
 
-    //  Array para capturar todos los errores
-    let errores= [];
-
     //Acción para validar el campo título al dejar el campo
-    inputSku.addEventListener("blur",()=>{
+    inputSku.addEventListener("blur",() => validations.sku(inputSku,errorSku))
+    inputTitle.addEventListener("blur",() => validations.title(inputTitle,errorTitle))
+    inputShortDescription.addEventListener("blur",() => validations.shortDescription(inputShortDescription,errorShortDescription))
+    inputLongDescription.addEventListener("blur",() => validations.longDescription( inputLongDescription,errorLongDescription))
+    inputRegularPrice.addEventListener("blur",() => validations.regularPrice( inputRegularPrice,errorRegularPrice))
+    inputDiscount.addEventListener("blur",() => validations.discount( inputDiscount,errorDiscount))
+    inputFee_q.addEventListener("blur",() => validations.fee_q( inputFee_q,errorFee_q))
+    inputTags.addEventListener("blur",() => validations.tags( inputTags,errorTags))
 
-        if (inputSku.value == ""){
-            errores.push("El SKU no puede estar vacío")
-            errorSku.classList.add("danger")
-            errorSku.innerHTML="El SKU no puede estar vacío"
-        }else{
-            if (inputSku.value.length < 6){
-                errores.push("El SKU no puede tener menos de 6 caracteres")
-                errorSku.classList.add("danger")
-                errorSku.innerHTML="El SKU no puede tener menos de 6 caracteres"
-            }else{
-                errorSku.style.display="none"
-            }
-        }
-    })
+    inputCategories.forEach( item => {
+        // let isChecked = false;
+        item.addEventListener('blur', () => {
+            validations.multiple(inputCategories, errorCategories);
+        })
+    }),
 
-    inputTitle.addEventListener("blur",()=>{
+    inputImage_url.addEventListener('blur', () => validations.image(inputImage_url, errorImage_url))
 
-        if (inputTitle.value == ""){
-            errores.push("El título no puede estar vacío")
-            errorTitle.classList.add("danger")
-            errorTitle.innerHTML="El título no puede estar vacío"
-        }else{
-            if (inputTitle.value.length < 5){
-                errores.push("El título no puede tener menos de 5 caracteres")
-                errorTitle.classList.add("danger")
-                errorTitle.innerHTML="El título no puede tener menos de 5 caracteres"
-            }else{
-                errorTitle.style.display="none"
-            }
-        }
-    })
-
-    inputShortDescription.addEventListener("blur",()=>{
-
-        if (inputShortDescription.value == ""){
-            errores.push("La descripción no puede estar vacía")
-            errorShortDescription.classList.add("danger")
-            errorShortDescription.innerHTML="La descripción no puede estar vacía"
-        }else{
-            if (inputShortDescription.value.length < 20){
-                errores.push("La descripción no puede tener menos de 20 caracteres")
-                errorShortDescription.classList.add("danger")
-                errorShortDescription.innerHTML="La descripción no puede tener menos de 20 caracteres"
-            }else{
-                errorShortDescription.style.display="none"
-            }
-        }
-    })
-
-    inputLongDescription.addEventListener("blur",()=>{
-
-        if (inputLongDescription.value == ""){
-            errores.push("La descripción no puede estar vacía")
-            errorLongDescription.classList.add("danger")
-            errorLongDescription.innerHTML="La descripción no puede estar vacía"
-        }else{
-            if (inputLongDescription.value.length < 20){
-                errores.push("La descripción no puede tener menos de 20 caracteres")
-                errorLongDescription.classList.add("danger")
-                errorLongDescription.innerHTML="La descripción no puede tener menos de 20 caracteres"
-            }else{
-                errorLongDescription.style.display="none"
-            }
-        }
-    })
-
-
-    inputRegularPrice.addEventListener("blur",()=>{
-
-        if (inputRegularPrice.value == ""){
-            errores.push("El precio no puede estar vacío")
-            errorRegularPrice.classList.add("danger")
-            errorRegularPrice.innerHTML="El precio no puede estar vacío"
-        }else{
-            if (parseInt(inputRegularPrice.value,10) < 0){
-                errores.push("El precio no puede ser negativo")
-                errorRegularPrice.classList.add("danger")
-                errorRegularPrice.innerHTML="El precio no puede ser negativo"
-            }else{
-                errorRegularPrice.style.display="none"
-            }
-        }
-    })
-
-    inputDiscount.addEventListener("blur",()=>{
-
-        if (parseInt(inputDiscount.value) < 0){
-            errores.push("El descuento no puede ser negativo")
-            errorDiscount.classList.add("danger")
-            errorDiscount.innerHTML="El descuento no puede ser negativo"
-        }else{
-            errorDiscount.style.display="none"
-        }
-    })
-
-    inputFee_q.addEventListener("blur",()=>{
-
-        if (parseInt(inputFee_q.value) < 0){
-            errores.push("Las cuotas no pueden ser negativas")
-            errorFee_q.classList.add("danger")
-            errorFee_q.innerHTML="Las cuotas no pueden ser negativas"
-        }else{
-            errorFee_q.style.display="none"
-        }
-    })
-
-    inputTags.addEventListener("blur",()=>{
-
-        if (inputTags.value == ""){
-            errores.push("Las etiquetas no pueden estar vacías")
-            errorTags.classList.add("danger")
-            errorTags.innerHTML="Las etiquetas no pueden estar vacías"
-        }else{
-            errorTags.style.display="none"
-            }
-    })
-
-    //Para validar checkboxes con el evento submit
-    var checkBoxes = form.querySelectorAll('input[type="checkbox"]'); // get all the check box
-    // document.getElementById('submit').addEventListener('click', getData); //add a click event to the save button
-    inputImage_url.addEventListener('click', getData); //add a click event to the save button
-
-    let errorCat = true;
-
-    function getData() { // this function will get called when the save button is clicked
-        checkBoxes.forEach(item => { // loop all the checkbox item
-            if (item.checked) {  //if the check box is checked
-                errorCat = false;
-            }
-        });
+    form.addEventListener("submit",e => {
         
-        if (errorCat) {
-            errores.push("Debes seleccionar al menos una categoría")
-            errorCategories.classList.add("danger")
-            errorCategories.innerHTML="Debes seleccionar al menos una categoría"
-        }else{
-            errorCategories.style.display="none"
-            }
-    }
+        //Reccoremos todos los campos para ver su validacion
+        allInputs.forEach( inputElement => inputElement.focus());
 
+        const allErrors = document.querySelectorAll('small.danger');
+        if ( allErrors.length > 0 ) e.preventDefault();
 
-    //PENDIENTES
-    //VALIDAR CATEGORIAS ("Debes seleccionar al menos una categoría")
-    // PREVENTDEFAULT NO FUNCIONA
-    
-    // form.addEventListener("submit", (e) => {
+        // allTextAreas.forEach( inputElement => inputElement.focus());
 
-        // if (inputCategories.value == ""){
-        //     errores.push("Debes seleccionar al menos una categoría")
-        //     errorCategories.classList.add("danger")
-        //     errorCategories.innerHTML="Debes seleccionar al menos una categoría"
-        // }else{
-        //     errorCategories.style.display="none"
-        //     }
-        
-        // if (errores.length > 0) {
-            // e.preventDefault();
-            // console.log("haciendo submit");
-        // }
-    // })
+        // const allErrors2 = document.querySelectorAll('small.danger');
+        // if ( allErrors2.length > 0 ) e.preventDefault();
+
+    })
 
 })
 
+function displayMessage( item, message ) {
+    item.style.display = "block";
+    item.classList.add("danger");
+    item.innerHTML = message;
+}
+
+function deleteError(item){
+    item.classList.remove('danger');
+    item.style.display = "none";
+}
 
