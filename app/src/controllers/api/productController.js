6f1cc56953d329134d11bb5c6304c03f772b96c9
Santi-Chaves <sjchaves97@gmail.com>
2022-09-17 17:01:47 -0300

    SC / PG : endpoint products detail

--- app/src/controllers/api/productController.js
+++ app/src/controllers/api/productController.js
@@ -30,8 +30,34 @@ const apiProductController = {
         }
     },
     detail: async (req,res) => {
-        res.send("hola products")
-    }
+        try {
+            //Hago los pedidos a la Base de Datos
+            const product = await queries.Product.find(req.params.id);
+
+            return res.status(200).json({
+                id: product.dataValues.id,
+                sku: product.dataValues.sku,
+                product_name: product.dataValues.product_name,
+                short_description: product.dataValues.short_description,
+                long_description: product.dataValues.long_description,
+                regular_price: product.dataValues.regular_price,
+                discount: product.dataValues.discount,
+                fee_q: product.dataValues.fee_q,
+                tags: product.dataValues.tags,
+                is_offer: product.dataValues.is_offer,
+                is_physical: product.dataValues.is_physical,
+                image_url: product.dataValues.image_url,
+                visits_q: product.dataValues.visits_q,
+                sales_q: product.dataValues.sales_q,
+                best_seller: product.dataValues.best_seller,
+                categories: product.categories.map( category => category.name),
+            })
+        } catch (e) {
+            //Si hay algun error, los atajo y muestro todo vacio
+            console.log('error,' , e);
+            return res.status(400).json(e);
+        }
+    },
 }
 
 module.exports = apiProductController;
